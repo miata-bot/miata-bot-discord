059dc30ffd7c8e4c5dcd377ad6981422806ffd8d
Connor Rigby <konnorrigby@gmail.com>
2019-05-22 13:03:10 -0700

    Set port for real

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,7 @@ config :miata_bot, MiataBot.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :miata_bot, MiataBot.Web.Endpoint,
-  port: String.to_integer(System.get_env("PORT") || "4001"),
+  port: System.get_env("PORT"),
   url: System.get_env("APP_URL")
 
 # config :logger,
