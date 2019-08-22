4651be1903ef1e0adaebf7b8a0d81be36fdb807e
Connor Rigby <konnorrigby@gmail.com>
2019-08-21 17:50:20 -0700

    let me google that for you

--- lib/miata_bot/discord.ex
+++ lib/miata_bot/discord.ex
@@ -206,6 +206,19 @@ defmodule MiataBot.Discord do
     handle_command(command, message)
   end
 
+  def handle_event(
+        {:MESSAGE_CREATE, {%{content: "!google " <> search, channel_id: channel_id}}, _state}
+      ) do
+    q = URI.encode_query(%{q: search, iee: 1})
+    lmgtfy = "https://lmgtfy.com/?#{q}"
+
+    embed =
+      Embed.put_url(%Embed{}, lmgtfy)
+      |> Embed.put_title("let me google that for you")
+
+    Api.create_message!(channel_id, embed: embed)
+  end
+
   def handle_event({:MESSAGE_CREATE, {%{channel_id: @verification_channel_id} = message}, _state}) do
     case message.attachments do
       [%{url: url} | _rest] ->
