8a737db042162326d898ae2f0f7564f126b7c50c
Connor Rigby <connorr@hey.com>
2021-03-10 11:35:00 -0800

    change herc own thing to not be so repetitive

--- lib/miata_bot_discord/guild/consumers/herc_consumer.ex
+++ lib/miata_bot_discord/guild/consumers/herc_consumer.ex
@@ -8,6 +8,21 @@ defmodule MiataBotDiscord.Guild.HercConsumer do
   import MiataBotDiscord.Guild.Registry, only: [via: 2]
   alias MiataBotDiscord.Guild.EventDispatcher
 
+  defmodule BritishSpellings do
+    use Tesla
+    plug Tesla.Middleware.JSON, decode_content_types: ["text/plain"]
+
+    plug Tesla.Middleware.BaseUrl,
+         "https://raw.githubusercontent.com/hyperreality/American-British-English-Translator/master/data/"
+
+    def british_spellings do
+      case get!("/british_spellings.json") do
+        %{status: 200, body: body} -> {:ok, body}
+        _ -> {:error, "failed to get spellings"}
+      end
+    end
+  end
+
   @doc false
   def start_link({guild, config, current_user}) do
     GenStage.start_link(__MODULE__, {guild, config, current_user}, name: via(guild, __MODULE__))
@@ -15,8 +30,21 @@ defmodule MiataBotDiscord.Guild.HercConsumer do
 
   @impl GenStage
   def init({guild, config, current_user}) do
-    {:producer_consumer, %{guild: guild, current_user: current_user, config: config},
+    send(self(), :get_british_spellings)
+
+    {:producer_consumer,
+     %{guild: guild, current_user: current_user, config: config, british_spellings: %{}},
      subscribe_to: [via(guild, EventDispatcher)]}
+  end
+
+  def handle_info(:get_british_spellings, state) do
+    case BritishSpellings.british_spellings() do
+      {:ok, data} ->
+        {:noreply, [], %{state | british_spellings: data}}
+
+      {:error, _} ->
+        {:noreply, [], state}
+    end
   end
 
   @impl GenStage
@@ -39,7 +67,6 @@ defmodule MiataBotDiscord.Guild.HercConsumer do
   end
 
   def handle_message(message, {actions, state}) do
-    # just making some random change.....
     if(message.author.id == 226_052_366_745_600_000) do
       # if(message.author.id == 316_741_621_498_511_363) do
       handle_herc(message, actions, state)
@@ -49,57 +76,69 @@ defmodule MiataBotDiscord.Guild.HercConsumer do
   end
 
   def handle_herc(message, actions, state) do
-    case process_content(message) do
+    case process_content(message, state.british_spellings) do
       {:ok, action} -> {actions ++ [action], state}
       _ -> {actions, state}
     end
   end
 
-  def process_content(%Nostrum.Struct.Message{content: content} = message)
+  def process_content(%Nostrum.Struct.Message{content: content} = message, british_spellings)
       when is_binary(content) do
-    cond do
-      String.contains?(String.downcase(content), "slushmatic") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "poorsche") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "saloon") -> own_herc_action(message)
-      String.contains?(String.downcase(content), " boot ") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "bonnet") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "colour") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "ferd") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "tyre") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "mommytank") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "windscreen") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "flavour") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "bloody") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "twat") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "winge") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "carpark") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "theatre") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "petrol") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "motoring") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "bloke") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "lad") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "bollock") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "wanker") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "humour") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "flavour") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "labour") -> own_herc_action(message)
-      String.contains?(String.downcase(content), "neighbour") -> own_herc_action(message)
-      true -> nil
+    murican_spelling =
+      Enum.find(String.split(content, " "), fn word ->
+        word = String.trim(String.downcase(word))
+        british_spellings[word] || hercism?(word)
+      end)
+
+    if murican_spelling do
+      own_herc_action(message, murican_spelling)
     end
   end
 
-  def process_content(%Nostrum.Struct.Message{}), do: nil
+  def process_content(%Nostrum.Struct.Message{}, _), do: nil
 
-  def own_herc_action(%Nostrum.Struct.Message{channel_id: channel_id, author: author}) do
-    message =
-      Enum.random([
-        "You live in San Francisco. Stop.",
-        "Hear that one on Top Gear?",
-        "Hitler would be proud of that one",
-        "You kiss your *mum* with that mouth?",
-        "https://cdn.discordapp.com/attachments/643947340453118019/814250327019880448/edsy7kdy7yf61.jpg"
-      ])
+  def own_herc_action(
+        %Nostrum.Struct.Message{channel_id: channel_id, author: _author, id: message_id},
+        _murican_spelling
+      ) do
+    emoji = %Nostrum.Struct.Emoji{
+      animated: false,
+      id: 819_288_827_419_033_601,
+      managed: false,
+      name: "okbritish",
+      require_colons: true,
+      roles: [],
+      user: nil
+    }
 
-    {:ok, {:create_message!, [channel_id, "#{Nostrum.Struct.User.mention(author)} #{message}"]}}
+    {:ok, {:create_reaction, [channel_id, message_id, emoji]}}
   end
+
+  def hercism?("slushmatic"), do: true
+  def hercism?("poorsche"), do: true
+  def hercism?("saloon"), do: true
+  def hercism?(" boot "), do: true
+  def hercism?("bonnet"), do: true
+  def hercism?("colour"), do: true
+  def hercism?("ferd"), do: true
+  def hercism?("tyre"), do: true
+  def hercism?("mommytank"), do: true
+  def hercism?("windscreen"), do: true
+  def hercism?("flavour"), do: true
+  def hercism?("bloody"), do: true
+  def hercism?("twat"), do: true
+  def hercism?("winge"), do: true
+  def hercism?("carpark"), do: true
+  def hercism?("theatre"), do: true
+  def hercism?("petrol"), do: true
+  def hercism?("motoring"), do: true
+  def hercism?("bloke"), do: true
+  def hercism?("lad"), do: true
+  def hercism?("bollock"), do: true
+  def hercism?("wanker"), do: true
+  def hercism?("humour"), do: true
+  def hercism?("labour"), do: true
+  def hercism?("neighbour"), do: true
+  def hercism?("4pot"), do: true
+  def hercism?(_), do: false
 end
