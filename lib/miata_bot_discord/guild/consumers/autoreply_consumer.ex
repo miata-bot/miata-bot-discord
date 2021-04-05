1398d27b00a7fdbfe174450637fbe3e1ca033f1c
Connor Rigby <connorr@hey.com>
2021-04-05 07:10:09 -0700

    idk

--- lib/miata_bot_discord/guild/consumers/autoreply_consumer.ex
+++ lib/miata_bot_discord/guild/consumers/autoreply_consumer.ex
@@ -29,10 +29,6 @@ defmodule MiataBotDiscord.Guild.AutoreplyConsumer do
         when author_id == current_user_id ->
           {actions, state}
 
-        {:MESSAGE_CREATE, message = %{author: %{id: author_id}}}, {actions, state}
-        when author_id == 407_680_002_658_795_535 ->
-          handle_annoying_guy(message, {actions, state})
-
         {:MESSAGE_CREATE, message}, {actions, state} ->
           handle_message(message, {actions, state})
 
@@ -42,22 +38,6 @@ defmodule MiataBotDiscord.Guild.AutoreplyConsumer do
 
     {:noreply, actions, state}
   end
-
-  def handle_annoying_guy(message, {actions, state}) do
-    actions =
-      actions ++
-        [
-          {:delete_message!, [message]}
-          # {:create_message!, [message.channel_id, "#{Nostrum.Struct.User.mention(message.author)} thought it wise to piss off an open source dev"]}
-        ]
-
-    {actions, state}
-  end
-
-  bang(
-    "!northstar",
-    "the developer of this bot has no idea what this means. sorry for the missed oppertunity."
-  )
 
   bang("ya rip", "https://www.youtube.com/watch?v=fKLmZNnMT0A")
   bang("yeah rip", "https://www.youtube.com/watch?v=fKLmZNnMT0A")
@@ -70,100 +50,13 @@ defmodule MiataBotDiscord.Guild.AutoreplyConsumer do
   )
 
   bang(
-    "!hercroast",
-    "https://cdn.discordapp.com/attachments/500143495043088395/590654628115382277/032216_Fire_Car_AB.jpg"
-  )
-
-  bang(
     "!longintake",
     "https://cdn.discordapp.com/attachments/384483113985900544/592810948201545739/IMG_20190613_153900.jpg"
   )
 
   bang(
-    "!18swapintake",
-    "https://media.discordapp.net/attachments/322080529245798401/593511885664550921/IMG_1140.jpg"
-  )
-
-  bang(
-    "!torquespecs",
-    "https://www.miata.net/garage/torque.html"
-  )
-
-  bang(
-    "!fartbeard",
-    "https://cdn.discordapp.com/attachments/322162421156282369/593854324891713546/image0.jpg"
-  )
-
-  bang(
-    "!bmwroast",
-    "https://media.discordapp.net/attachments/322162421156282369/593869644365037628/b49d82b0ad.png?width=590&height=658"
-  )
-
-  bang("!e", "https://i.ytimg.com/vi/J6NioWmscDc/maxresdefault.jpg")
-
-  bang(
-    "!flroast",
-    "https://cdn.discordapp.com/attachments/322162421156282369/593905029094572032/florida.gif"
-  )
-
-  bang(
-    "!floridiaroast",
-    "https://cdn.discordapp.com/attachments/322162421156282369/593905029094572032/florida.gif"
-  )
-
-  bang(
-    "!boing",
-    "https://www.youtube.com/watch?v=hoS1MCF8AeI"
-  )
-
-  bang(
-    "!weed",
-    "https://www.youtube.com/watch?v=5lEelz0iUJo"
-  )
-
-  bang(
     "!doot",
     "https://youtu.be/eVrYbKBrI7o"
-  )
-
-  bang(
-    "!nou",
-    "https://cdn.discordapp.com/attachments/351767273712910336/594569216640942083/GJElD4jJUQ2xAPwjRIjBcUeAf311aN1oa791xzRpQbVJx1oF-zjoQZ1Sq1R_JpV31jSBPOJ9WvIQvhFDVpp7Cwoaye4yR0VxEOsy.png"
-  )
-
-  bang(
-    "!rotary",
-    "https://www.youtube.com/watch?v=FZUcpVmEHuk"
-  )
-
-  bang(
-    "!fca",
-    "https://www.youtube.com/watch?v=FZUcpVmEHuk"
-  )
-
-  bang(
-    "!FCA",
-    "https://www.youtube.com/watch?v=FZUcpVmEHuk"
-  )
-
-  bang(
-    "!potato",
-    "https://cdn.discordapp.com/attachments/591751583767724042/594582309639028745/unknown.png"
-  )
-
-  bang(
-    "!playstation",
-    "https://youtu.be/oAhvQoLpvsM"
-  )
-
-  bang(
-    "!theresyourproblem",
-    "https://i.kym-cdn.com/photos/images/newsfeed/000/228/269/demotivational-posters-theres-your-problem.jpg"
-  )
-
-  bang(
-    "!spaghetti",
-    "https://media.discordapp.net/attachments/322080529245798401/611652555037999124/RZ1AVzfrlMtElpqJQLAYfshvvmll12ED3q9MILYBYDESQeWyXiCLu0-93k76UCBtN9RFGjMaFoNme8whKXqXQXTZMWSeR4F4K8hS.png?width=494&height=659"
   )
 
   bang(
