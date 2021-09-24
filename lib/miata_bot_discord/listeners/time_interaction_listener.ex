4ae4c6974b2b4348aec3d2fd2bd2e878d5a27c04
Connor Rigby <connorr@hey.com>
2021-09-24 13:36:08 -0600

    Add a lil error handling

--- lib/miata_bot_discord/listeners/time_interaction_listener.ex
+++ lib/miata_bot_discord/listeners/time_interaction_listener.ex
@@ -16,10 +16,20 @@ defmodule MiataBotDiscord.TimeInteractionListener do
         %Interaction{data: %{name: "time", options: [%{name: "user", value: discord_user_id}]}} = iaction,
         state
       ) do
-    {:ok, %MiataBot.Partpicker.User{preferred_timezone: tz}} = MiataBot.Partpicker.user(discord_user_id)
-    {:ok, shifted} = DateTime.shift_zone(DateTime.utc_now(), tz)
-    response = %{type: 4, data: %{content: "#{shifted}"}}
-    create_interaction_response(iaction, response)
+    with {:ok, %MiataBot.Partpicker.User{preferred_timezone: tz}} <- MiataBot.Partpicker.user(discord_user_id),
+         {:ok, shifted} <- DateTime.shift_zone(DateTime.utc_now(), tz) do
+      response = %{type: 4, data: %{content: "#{shifted}"}}
+      create_interaction_response(iaction, response)
+    else
+      {:error, :time_zone_not_found} ->
+        response = %{type: 4, data: %{content: "that user has not configured their timezone. maybe ping them about it"}}
+        create_interaction_response(iaction, response)
+
+      error ->
+        response = %{type: 4, data: %{content: "error getting time for user: #{inspect(error)}"}}
+        create_interaction_response(iaction, response)
+    end
+
     {:noreply, state}
   end
 
